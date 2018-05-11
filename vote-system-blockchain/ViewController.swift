83058af68ae043f93fcb6b0e1fc514ebc8b9a07b
Matheus Amancio <matheusamancio23@hotmail.com>
2018-05-11 00:54:43 -0300

    start project

---
+++ vote-system-blockchain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vote-system-blockchain
+//
+//  Created by Matheus Amancio Seixeiro on 11/05/2018.
+//  Copyright © 2018 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
