d281b75fe74c6023552bac5a285d37e707fd6189
Matheus Amancio <matheusamancio23@hotmail.com>
2018-05-12 16:26:37 -0300

    Create the UI of CPF screen

--- vote-system-blockchain/FontStyles.swift
+++ vote-system-blockchain/FontStyles.swift
@@ -13,7 +13,7 @@ import UIKit
 extension UIFont {
     static let title: UIFont = UIFont.boldSystemFont(ofSize: 30)
     static let subtitle: UIFont = systemFont(ofSize: 20)
-    static let textField: UIFont = UIFont.boldSystemFont(ofSize: 25)
+    static let textField: UIFont = UIFont.boldSystemFont(ofSize: 30)
     static let button: UIFont = UIFont.boldSystemFont(ofSize: 15)
     static let text: UIFont = UIFont.systemFont(ofSize: 20)
     static let nameCandidate: UIFont = UIFont.boldSystemFont(ofSize: 17)
--- vote-system-blockchain/ViewController.swift
+++ vote-system-blockchain/ViewController.swift
@@ -21,11 +21,46 @@ class ViewController: BaseViewController {
         return l
     }()
     
+    private let headerLabel: UILabel = {
+        let label = UILabel.subtitle()
+        label.text = "Digite seu CPF"
+        label.textColor = UIColor.black
+        label.textAlignment = .left
+        return label
+    }()
+    private let cpfTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "123.456.789-00"
+        textfield.font = UIFont.textField
+        textfield.keyboardType = UIKeyboardType.numberPad
+        return textfield
+    }()
+    private let lineCpfView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    fileprivate let validateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Próxima Etapa", for: .normal)
+        button.setTitleColor(.white, for: UIControlState.normal)
+        button.titleLabel?.font = UIFont.button
+        button.titleLabel?.textAlignment = .center
+        button.backgroundColor = UIColor.gray
+        button.layer.cornerRadius = 4
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(titleLabel)
+        view.addSubview(headerLabel)
+        view.addSubview(cpfTextField)
+        view.addSubview(lineCpfView)
+        view.addSubview(cpfTextField)
+        view.addSubview(validateButton)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func setupConstraints() {
@@ -35,6 +70,36 @@ class ViewController: BaseViewController {
             make.left.equalToSuperview().offset(24)
             make.top.equalToSuperview().offset(107)
             make.right.equalToSuperview().offset(-24)
+        }
+        
+        // MARK: headerLabel
+        headerLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(44)
+            make.left.equalToSuperview().offset(35)
+            make.right.equalToSuperview().offset(-35)
+        }
+        
+        // MARK: cpfTextField
+        cpfTextField.snp.makeConstraints { (make) in
+            make.top.equalTo(headerLabel.snp.bottom)
+            make.left.equalToSuperview().offset(35)
+            make.right.equalToSuperview().offset(-35)
+            make.height.equalTo(44)
+        }
+        // MARK: lineCpfView
+        lineCpfView.snp.makeConstraints { make in
+            make.bottom.equalTo(cpfTextField.snp.bottom)
+            make.left.equalToSuperview().offset(35)
+            make.right.equalToSuperview().offset(-35)
+            make.height.equalTo(1)
+        }
+        
+        //MARK: validateButton
+        validateButton.snp.makeConstraints { (make) in
+            make.top.equalTo(lineCpfView.snp.bottom).offset(44)
+            make.left.equalToSuperview().offset(35)
+            make.right.equalToSuperview().offset(-35)
+            make.height.equalTo(50)
         }
         
     }
