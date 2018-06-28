a055c99da2f9c6f92500032f570f87ea1d65c9d1
Matheus Amancio <matheusamancio23@hotmail.com>
2018-06-27 21:14:56 -0300

    create user and candidate classes

---
+++ Models/Candidate.swift
@@ -0,0 +1,16 @@
+//
+//  Candidate.swift
+//  vote-system-blockchain
+//
+//  Created by Matheus Amancio Seixeiro on 27/06/2018.
+//  Copyright © 2018 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+import Foundation
+
+struct Candidate {
+    var name: String
+    var vice: String
+    var party: String
+    var photo: String
+}
---
+++ Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  vote-system-blockchain
+//
+//  Created by Matheus Amancio Seixeiro on 27/06/2018.
+//  Copyright © 2018 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+import Foundation
+
+struct User{
+    var CPF: Int
+    var gender: String
+    var sexual_choice: String
+    var age: Int
+    var curso: String
+}
