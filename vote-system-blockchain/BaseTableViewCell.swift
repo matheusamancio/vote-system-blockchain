56dd4b24c4ab43f7df95d60ca54e5a6241ae5d53
Matheus Amancio <matheusamancio23@hotmail.com>
2018-07-29 12:05:28 -0300

    change folder position of basetableviewcell

--- vote-system-blockchain/BaseTableViewCell.swift
+++ vote-system-blockchain/BaseTableViewCell.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2018 Matheus Amancio Seixeiro. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func initialize() {
+        // Override point
+    }
+    
+}
