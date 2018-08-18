5f42d8b78a339f385c380a721886b8a5f7406c40
Matheus Amancio <matheusamancio23@hotmail.com>
2018-08-17 22:38:38 -0300

    start first quiz controller

---
+++ vote-system-blockchain/Scenes/Quiz/FirstQuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstQuizViewController.swift
+//  vote-system-blockchain
+//
+//  Created by Matheus Amancio Seixeiro on 16/08/2018.
+//  Copyright © 2018 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+import UIKit
+
+class FirstQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
